7f44ecccc9226ad9d35e6cfc8f7fdcc03e287970
iurycar <iurycardoso2001@gmail.com>
2025-10-11 23:11:38 -0300

    Processamento dos status das tarefas

--- database/SOMENTE_TESTES_MYSQL.sql
+++ database/SOMENTE_TESTES_MYSQL.sql
@@ -6,8 +6,8 @@ describe `teams`;
 
 delete from `employee` where user_id = 'b8460203-63b6-49ff-85e2-9e3be1ea20f9';
 delete from `teams` where id_team = '806443c5-9271-464a-a1da-4581c7f766e4';
-delete from `sheet` where id_file = 'ea31255b-67bd-4e53-8094-290eecebcb22.xlsx';
-delete from `project` where id_file = 'ea31255b-67bd-4e53-8094-290eecebcb22.xlsx';
+delete from `sheet` where id_file = 'c17545cd-5cf4-4573-82f1-a163ff5d3a50.xlsx';
+delete from `project` where id_file = 'c17545cd-5cf4-4573-82f1-a163ff5d3a50.xlsx';
 
 
 alter table `sheet`
@@ -25,9 +25,20 @@ select * from `sheet`;
 select * from `employee`;
 select * from `teams`;
 
+select * from `sheet` where `id_file` = 'bb2dc2cb-2ff4-460a-8a81-bca8e0ceeb23.xlsx';
+
+UPDATE `sheet` SET `start_date` = NOW() WHERE (`start_date` IS NULL) AND (`conclusion` > 0 AND `conclusion` < 1);
+
+select `completed`, `id_file`, `project_name` from `project` where `id_team` = 'b80bf62a-6ff5-498e-9b92-12c9d197122d';
+select COUNT(*) from sheet where `id_file` = 'c17545cd-5cf4-4573-82f1-a163ff5d3a50.xlsx';
+
+SELECT COUNT(*) AS 'concluidas' FROM `sheet` WHERE `id_file` = 'c17545cd-5cf4-4573-82f1-a163ff5d3a50.xlsx' AND `conclusion` = 1;
+SELECT COUNT(*) AS 'overdue' FROM `sheet` WHERE `id_file` = 'c17545cd-5cf4-4573-82f1-a163ff5d3a50.xlsx' AND `conclusion` < 1 AND start_date < NOW();
+SELECT `start_date`, `duration`, `conclusion` FROM `sheet` WHERE `id_file` = 'c17545cd-5cf4-4573-82f1-a163ff5d3a50.xlsx' AND `start_date` IS NOT NULL AND `conclusion` < 1;
+
 select start_date from `sheet`;
 
-update `sheet` set `start_date` = '2025-09-29 12:03:00' where `id_task` = 312;
+update `sheet` set `start_date` = '2025-08-24 12:03:00' where `id_task` = 3283;
 
 select max(`num`) from `sheet`;
 
