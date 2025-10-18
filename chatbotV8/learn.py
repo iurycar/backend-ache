8a0cbf4cd560329b0727ce3470fc56ec55fadd8b
iurycar <iurycardoso2001@gmail.com>
2025-10-17 23:42:24 -0300

    Implementação do Algoritmo Aho-Corasick

--- chatbotV8/learn.py
+++ chatbotV8/learn.py
@@ -1,62 +1,36 @@
 from pathlib import Path
+import json
 
 diretorio = Path(__file__).parent
-caminho_termo = diretorio / "dados" / "termos_chaves.txt"
-caminho_resposta = diretorio / "dados" / "respostas.txt"
+caminho_termo = diretorio / "dados" / "termos_chaves.json"
+caminho_resposta = diretorio / "dados" / "respostas.json"
 
-def get_keywords() -> dict:
-    """ Função para acessar os dados treinados para perguntas no arquivo termos_chaves.txt.
+def get_keywords() -> dict[str, list[str]]:
+    """ Função para acessar os dados treinados para perguntas no arquivo termos_chaves.json.
     @return: Dicionário com palavras chaves e suas respectivas demandas."""
     # Verifica se o arquivo existe
     if caminho_termo.exists():
-        palavras_chave = {}
+        palavras_chave: dict[str, list[str]] = {}
 
         # Abre o arquino modo leitura e utiliza o sistema de codificação de caracteres "utf-8"
         with open(caminho_termo, "r", encoding="utf-8") as termos:
-            linhas = termos.readlines() # Ler o arquivo por linhas
-
-            # Passa por todas as linhas do arquivo
-            for i in range(len(linhas)):
-                palavra = linhas[i].replace("\n", "")
-                termo = palavra.split("::") # Separa o dado no '::' (ex. 'olá :: cumprimentar' -> ['olá ', ' cumprimentar'])
-
-                # Verifica se tem o termo e a demanda
-                if len(termo) != 1:
-                    chave = termo[0].strip() # Pega somente o termo e remove o espaço em branco
-                    demanda = termo[1].strip() # Pega somente a demanda e remove o espaço em branco
-
-                    palavras_chave[chave] = demanda
+            palavras_chave = json.load(termos)
 
         return palavras_chave
 
     else:
         print(f"Erro: O arquivo '{caminho_termo}' não encontrado.")
 
-def get_response_keywords() -> dict:
-    """ Função para acessar os dados treinados para respostas no arquivo respostas.txt.
+def get_response_keywords() -> dict[str, str | int]:
+    """ Função para acessar os dados treinados para respostas no arquivo respostas.json.
     @return: Dicionário com demandas e suas respectivas respostas."""
     
     # Verifica se o arquivo existe
     if caminho_resposta.exists():
-        responder_chaves = {}
+        responder_chaves: dict[str, str | int] = {}
 
         with open(caminho_resposta, "r", encoding="utf-8") as chaves:
-            linhas = chaves.readlines()
-            
-            for i in range(len(linhas)):
-                palavra = linhas[i].replace("\n", "")
-                termo = palavra.split("::")
-
-                if len(termo) != 1:
-                    chave = termo[0].strip()
-                    resposta_str = termo[1].strip()
-
-                    try:
-                        resposta = int(resposta_str)
-                    except:
-                        resposta = resposta_str
-
-                    responder_chaves[chave] = resposta
+            responder_chaves = json.load(chaves)
 
         return responder_chaves
     else:
@@ -86,32 +60,65 @@ def training() -> None:
 
         if treinamento == 1: # Treinar perguntas
             if visualizar == 1:
-                print("\n"+str(termos_chaves())+"\n")
+                print("\n"+str(get_keywords())+"\n")
 
             demanda = str(input("\nInforme qual a demanda da palavra chave (ex. para a demanda 'cumprimentar' a palavra chave pode ser 'olá'): "))
             print(f"\nPara as palavras chaves a resposta será '{demanda}'.")
             print(f"Lembre-se para novas demandas, o 'respostas_termos' deve ser treinado e para ações o arquivo organizador.py deve se configurado.")
 
-            with open(caminho_termo, "a", encoding="utf-8") as saida:
+            # Precisa abrir o arquivo .json no modo escrita e verificar se a palavra chave para demanda já existe
+            # Se não existir, adicionar a nova palavra chave para demanda, se existir, adicionar a palavra chave na lista de palavras chaves da demanda
+            with open(caminho_termo, "r+", encoding='utf-8') as arquivo:
+                dados = json.load(arquivo)
+
                 while True:
                     palavra = str(input("Informe a palavra chave (ou '0' para sair): "))
-
                     if palavra == '0':
                         break
+
+                    if demanda in dados:
+                        if palavra not in dados[demanda]:
+                            dados[demanda].append(palavra)
+                    else:
+                        dados[demanda] = [palavra]
+
+                    # Move o cursor para o início do arquivo para sobrescrever os dados
+                    arquivo.seek(0)
+                    json.dump(dados, arquivo, ensure_ascii=False, indent=4)
+                    arquivo.truncate()  # Remove qualquer dado remanescente no arquivo após a nova escrita
                     
-                    saida.write(f"{palavra} :: {demanda}\n")
+                    print(f"A palavra chave '{palavra}' será associada a demanda '{demanda}'.\n")
 
         elif treinamento == 2: # Treinar respostas
             if visualizar == 1:
-                print("\n"+str(respostas_termos()))
+                print("\n"+str(get_response_keywords()))
 
             demanda = str(input("\nInforme qual a demanda que deseja responder (ex. 'cumprimentar' ou '1' para ações): "))
             print(f"\nPara a demanda '{demanda}' a resposta pode ser uma frase (ex. 'Olá, como posso ajudar?') ou um dígito/int (ex. '1' para ações).")
             responder = str(input("Informe a resposta (ou '0' para sair): "))
             print(f"A resposta para a demanda '{demanda}' será '{responder}'.")
 
-            with open(caminho_resposta, "a", encoding='utf-8') as saida:
-                saida.write(f"{demanda} :: {responder}\n")
+            # Precisa abrir o arquivo .json no modo escrita e verificar se a demanda já existe
+            with open(caminho_resposta, "r+", encoding='utf-8') as arquivo:
+                dados = json.load(arquivo)
+
+                if responder == '0':
+                    break
+
+                # Tenta converter a resposta para int, se possível
+                try:
+                    resposta_convertida: str | int = int(responder)
+                except ValueError:
+                    resposta_convertida = responder
+
+                dados[demanda] = resposta_convertida
+
+                # Move o cursor para o início do arquivo para sobrescrever os dados
+                arquivo.seek(0)
+                json.dump(dados, arquivo, ensure_ascii=False, indent=4) # Sobrescreve os dados no arquivo
+                arquivo.truncate()  # Remove qualquer dado remanescente no arquivo após a nova escrita
+                
+                print(f"A demanda '{demanda}' terá a resposta '{responder}'.")
 
 
 if __name__ == '__main__':
